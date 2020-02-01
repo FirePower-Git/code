550bd212f7473a4ed1eb6201b5e1095c3b0efc25
FirePower-Git <58250403+FirePower-Git@users.noreply.github.com>
2020-02-01 16:58:37 +0100

    Update Main.java

--- java/Text Recognition/src/main/Main.java
+++ java/Text Recognition/src/main/Main.java
@@ -1,1 +1,17 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Main {
+	
+	ArrayList<Double> p = new ArrayList<Double>();
+	
+	public static void main(String[] args) {
 		AI.init();
+	}
+	
+	public static double NInput() {
+		return 0.f;
+	}
+
+}
