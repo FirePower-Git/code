426c50bd8e5c02787aa1bb963706a5071a83c53e
FirePower-Git <58250403+FirePower-Git@users.noreply.github.com>
2020-02-01 16:57:36 +0100

    Create AI.java

---
+++ java/Text Recognition/src/main/AI.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class AI {
+	
+	public static BufferedImage[] images = new BufferedImage[200];
+	
+	public static void init() {
+		
+		String[] s = new String[100];
+		
+		for (int i = 0; i < images.length; i++)
+			try {
+				images[i] = ImageIO.read(new File(s[i]));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	public static void AI() {
+		
+	}
+	
+}
